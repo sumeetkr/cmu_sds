bdfe7197c997304f243b88865bf85c67cb7d991a
Kaushik Gopal <code@kaush.co>
2012-11-29 16:01:40 -0800

    Creating Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmuSds::Application.config.secret_token = 'd364578e6f99afe1955f7b5fcf9f9c84a476a1aa1ea0e4e5849a59132e16728603ee7c1944dfcda34055f8bf2df03819744801ce4bcf8039dac74e23144677de'
