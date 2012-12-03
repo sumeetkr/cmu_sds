6029a466a22454533567d3287a180134c183a4ca
Kaushik Gopal <code@kaush.co>
2012-12-02 18:52:34 -0800

    added populate_sqldb method to sample_data.rake for populating sample relational info

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,5 +1,5 @@
 class Sensor < ActiveRecord::Base
-  attr_accessible :guid
+  attr_accessible :guid, :sensor_type_id, :min_value, :max_value, :device_guid, :device_id, :predecessor, :metadata, :gps_coord_lat, :gps_coord_long, :gps_coord_alt
 
   belongs_to :device
   belongs_to :sensor_type
